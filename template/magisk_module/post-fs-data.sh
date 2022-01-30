7de60f0a222f039ab449197b6699888425d8d703
Zapic <45936772+KawaiiZapic@users.noreply.github.com>
2022-01-31 00:20:57 +0800

    Update post-fs-data.sh

--- template/magisk_module/post-fs-data.sh
+++ template/magisk_module/post-fs-data.sh
@@ -15,14 +15,24 @@ fi
 
 ORIG_DIR=/data/misc/riru/modules/mipush_fake/packages
 TARGET_DIR=/data/misc/mph/config/packages
+PREFER_SYSTEM=0
 if [ -d "$ORIG_DIR" ] && [ -d "$TARGET_DIR" ];
 then
+  if [ -f "$TARGET_DIR"/prefer_system ];
+  then
+    PREFER_SYSTEM=1
+  fi
   rm "$TARGET_DIR"/* 2> /dev/null
   for package in "$ORIG_DIR"/*
-    do
-      rename="$(echo $(basename "$package") | sed -E "s/^[0-9]+.//g")"
-      cp $package "$TARGET_DIR"/$rename
-    done
+  do
+    rename="$(echo $(basename "$package") | sed -E "s/^[0-9]+.//g")"
+    cp $package "$TARGET_DIR"/$rename
+  done
+  if [ PREFER_SYSTEM -eq 1];
+  then
+    touch "$TARGET_DIR"/prefer_system
+  fi
+  rm "$ORIG_DIR"/prefer_system 2> /dev/null
   set_perm_recursive "$TARGET_DIR" 0 0 0755 0644
   chcon -R u:object_r:magisk_file:s0 "$TARGET_DIR"
 fi
