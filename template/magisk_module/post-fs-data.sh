c5972f2930995c82936faecf1dce58586372629c
Zapic <45936772+KawaiiZapic@users.noreply.github.com>
2022-01-30 23:00:03 +0800

    Update post-fs-data.sh

--- template/magisk_module/post-fs-data.sh
+++ template/magisk_module/post-fs-data.sh
@@ -12,3 +12,14 @@ else
   # In case user downgrade Riru to pre-v24
   [ -d "$MODDIR"/riru ] && mv "$MODDIR"/riru "$MODDIR"/system
 fi
+
+ORIG_DIR=/data/misc/riru/modules/mipush_fake/packages
+TARGET_DIR= /data/misc/mph/config/packages
+rm "$ORIG_DIR/*"
+for package in "$ORIG_DIR/*"
+do
+  rename="$(echo "$package" | sed -e 's/^[0-9]\+.//g')"
+  cp "$ORIG_DIR/$package" "$TARGET_DIR/$rename"
+done
+set_perm_recursive "$TARGET_DIR" 0 0 0755 0644
+chcon -R u:object_r:magisk_file:s0 "$TARGET_DIR"
